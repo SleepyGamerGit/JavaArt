3adb34d3cd0f34f8a4f4ed61741005e64f474c7b
Sleepy <hillsteadm@gmail.com>
2019-05-03 09:25:04 -0600

    Added framework for GUI

--- src/art/view/ArtFrame.java
+++ src/art/view/ArtFrame.java
@@ -1,6 +1,21 @@
 package art.view;
 
-public class ArtFrame
-{
+import javax.swing.JFrame;
+import art.controller.ArtController;
 
+public class ArtFrame extends JFrame
+{
+	private ArtController app;
+	
+	public ArtFrame()
+	{
+		super();
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		
+	}
 }
