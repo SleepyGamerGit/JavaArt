58f921801bc6d9e7844f8798c79e3bc093c3a073
Sleepy <hillsteadm@gmail.com>
2019-05-09 08:53:45 -0600

    making it so you can draw dots

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -1,12 +1,24 @@
 package art.view;
 
 import art.controller.ArtController;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.util.Hashtable;
+import java.awt.*;
+
 import javax.swing.*;
 
 public class ArtPanel extends JPanel
 {
 	private ArtController app;
-	
+	private DrawingPanel canvas;
+	private JPanel colorPanel;
+	private JPanel menuPanel;
+	private JScorllPane canvasPane;
+	private SpringLayout applayout;
+	private JSlider widthSlider;
 	private JButton redButton;
 	private JButton blueButton;
 	private JButton yellowButton;
@@ -14,12 +26,19 @@ public class ArtPanel extends JPanel
 	private JButton greenButton;
 	private JButton violetButton;
 	private JButton magentaButton;
-	
+	private JButton saveButton;
+	private JButton loadButton;
+	private JButton clearButton;
+
+
+	private final int MINIMUM_LINE = 1;
+	private final int MAXIMUM_LINE = 25;
+
 	public ArtPanel(ArtController app)
 	{
 		super();
 		this.app = app;
-		
+
 		redButton = new JButton("Red");
 		blueButton = new JButton("Blue");
 		yellowButton = new JButton("Yellow");
@@ -27,42 +46,128 @@ public class ArtPanel extends JPanel
 		greenButton = new JButton("Green");
 		violetButton = new JButton("Violet");
 		magentaButton = new JButton("Magenta");
-		
+
+		saveButton = new JButton("save panel");
+		loadButton = new JButton("load new panel");
+		clearButton = new JButton("clear panel");
+		widthSlider = new JSlider(MINIMUM_LINE, MAXIMUM_LINE);
+		colorPanel = new JPanel(new GridLayout(0, 1));
+		menuPanel = new JPanel(new GridLayout(0, 1));
+
 		setupMenuPanel();
 		setupPanel();
 		setupLayout();
 		setupListeners();
 		setupScrollPane();
 	}
-	
+
+	private void setupSlider()
+	{
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
+		scaleLabels.put(MINIMUM_LINE, new JLabel("<HTML>Small<BR>Line</HTML>"));
+		scaleLabels.put(MAXIMUM_LINE, new JLabel("<HTML>Small<BR>Line</HTML>"));
+		widthSlider.setLabelTable(scaleLabels);
+		widthSlider.setSnapToTicks(true);
+		widthSlider.setMajorTickSpacing(5);
+		widthSlider.setMinorTickSpacing(1);
+		widthSlider.setPaintTicks(true);
+		widthSlider.setPaintLabels(true);
+		widthSlider.setValue((MAXIMUM_LINE + MINIMUM_LINE) / 2);
+	}
+
+	private void setupMenuPanels()
+	{
+
+	}
+
 	private void setupMenuPanel()
 	{
-		this.add(redButton);
-		this.add(blueButton);
-		this.add(yellowButton);
-		this.add(orangeButton);
-		this.add(greenButton);
-		this.add(violetButton);
-		this.add(magentaButton);
+		colorPanel.setPreferredSize(new Dimension(50, 700));
+		menuPanel.setPreferredSize(new Dimension(50, 700));
+
+		violetButton.setForeground(new Color(75, 0, 130));
+		blueButton.setForeground(Color.BLUE);
+		greenButton.setForeground(Color.GREEN);
+		orangeButton.setForeground(Color.ORANGE);
+		redButton.setForeground(Color.RED);
+		yellowButton.setForeground(Color.YELLOW);
+
+		colorPanel.add(redButton);
+		colorPanel.add(blueButton);
+		colorPanel.add(yellowButton);
+		colorPanel.add(orangeButton);
+		colorPanel.add(greenButton);
+		colorPanel.add(violetButton);
+		colorPanel.add(magentaButton);
+
+		menuPanel.add(widthSlider);
+		menuPanel.add(saveButton);
+		menuPanel.add(loadButton);
+		menuPanel.add(clearButton);
 	}
-	
+
 	private void setupScrollPane()
 	{
-		
+		canvasPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		canvasPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		canvasPane.setViewportView(canvas);
 	}
-	
+
 	private void setupPanel()
 	{
-		
+
 	}
-	
+
 	private void setupLayout()
 	{
-		
+
 	}
-	
+
 	private void setupListeners()
 	{
+		canvas.addMouseListener(new MouseListener()
+				{
+					public void mouseClicked(MouseEvent e)
+					{
+						canvas.drawDot(e.getX(), e.getY(), widthSlider.getValue());
+					}
+
+					public void mousePressed(MouseEvent e)
+					{
+						
+					}
+					
+					public void mouseReleased(MouseEvent e)
+					{
+						canvas.resetPoint();
+					}
+					
+					public void mouseEntered(MouseEvent e)
+					{
+						canvas.resetPoint();
+					}
+					
+					public void mouseExited(MouseEvent e)
+					{
+						canvas.resetPoint();
+					}
+					
+				});
+		
+		canvas.addMouseMotionListener(new MousMotionListener()
+				{
+				
+				public void mouseDragged(MouseEvent e)
+				{
+				
+				}
+				
+				public void mouseMoved(MouseEvent e)
+				{
+					
+				}
+				
+				});
 		
 	}
 }
--- src/art/view/DrawingPanel.java
+++ src/art/view/DrawingPanel.java
@@ -2,15 +2,106 @@ package art.view;
 
 import javax.swing.JPanel;
 import art.controller.ArtController;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class DrawingPanel extends JPanel
 {
 	private ArtController app;
-	
+	private Color currentColor;
+	private BufferedImage currentCanvas;
+	private int previousX;
+	private int previousY;
+
 	public DrawingPanel(ArtController app)
 	{
 		super();
 		this.app = app;
+		this.currentCanvas = new BufferedImage(700, 700, BufferedImage.TYPE_INT_ARGB);
+		setupPanel();
+		resetPoint();
 	}
+
+	public void resetPoint()
+	{
+		previousX = Integer.MIN_VALUE;
+		previousY = Integer.MIN_VALUE;
+	}
+
+	public void setupPanel()
+	{
+		this.setPreferredSize(new Dimension(700, 700));
+		this.setBackground(Color.MAGENTA);
+		this.currentColor = Color.GREEN;
+	}
+
+	@Override
+	protected void paintComponent(Graphics graphics)
+	{
+		super.paintComponent(graphics);
+		graphics.drawImage(currentCanvas, 0, 0, null);
+	}
+
+	private Color randomColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		int alpha = (int) (Math.random() * 256);
+
+		Color randomColor = new Color(red, green, blue, alpha);
+		return randomColor;
+	}
+
+	public void setCurrentColor(String color)
+	{
+		if (color.equalsIgnoreCase("Red"))
+		{
+			currentColor = Color.RED;
+		}
+		else if (color.equalsIgnoreCase("blue"))
+		{
+			currentColor = Color.BLUE;
+		}
+		else if (color.equalsIgnoreCase("yellow"))
+		{
+			currentColor = Color.YELLOW;
+		}
+		else if (color.equalsIgnoreCase("Orange"))
+		{
+			currentColor = Color.ORANGE;
+		}
+		else if (color.equalsIgnoreCase("green"))
+		{
+			currentColor = Color.GREEN;
+		}
+		else if (color.equalsIgnoreCase("violet"))
+		{
+			currentColor = new Color(75, 0, 130);
+		}
+		else if (color.equalsIgnoreCase("magenta"))
+		{
+			currentColor = Color.MAGENTA;
+		}
+		else
+		{
+			currentColor = randomColor();
+		}
+	}
+
+	public void drawDot(int currentX, int currentY, int width)
+	{
+		Graphics2D current = currentCanvas.createGraphics();
+		current.setColor(currentColor);
+		current.setStroke(new BasicStroke(width));
+		current.drawOval(currentX, currentY, width, width);
+		repaint();
+	}
+	
 	
 }
